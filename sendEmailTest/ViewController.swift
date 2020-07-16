42d2c15102db61e4eb00bb2b2ec9a6c8683d7c7e
Ginger <gingerhusunny@gmail.com>
2020-07-16 10:09:27 -0700

    sendEmailTest1

--- sendEmailTest/ViewController.swift
+++ sendEmailTest/ViewController.swift
@@ -7,14 +7,117 @@
 //
 
 import UIKit
+import MessageUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MFMailComposeViewControllerDelegate, UITextFieldDelegate, UITextViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        subject.delegate = self
+        body.delegate = self
     }
 
+    @IBOutlet weak var subject: UITextField!
+    
+    @IBOutlet weak var body: UITextView!
+    
+    @IBAction func sendMail(_ sender: Any) {
+        let picker = MFMailComposeViewController()
+             picker.mailComposeDelegate = self
+                 
+             if let subjectText = subject.text {
+                 picker.setSubject(subjectText)
+             }
+             picker.setMessageBody(body.text, isHTML: true)
+                 
+             present(picker, animated: true, completion: nil)
+    }
+//    
+//    @IBAction func sendMail(_ sender: Any) {
+//        let picker = MFMailComposeViewController()
+//        picker.mailComposeDelegate = self
+//            
+//        if let subjectText = subject.text {
+//            picker.setSubject(subjectText)
+//        }
+//        picker.setMessageBody(body.text, isHTML: true)
+//            
+//        present(picker, animated: true, completion: nil)
+//    }
+
+    // MFMailComposeViewControllerDelegate
+
+    // 1
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+         dismiss(animated: true, completion: nil)
+    }
+
+    // UITextFieldDelegate
+        
+    // 2
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+            
+        return true
+    }
+        
+    // UITextViewDelegate
+        
+    // 3
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        body.text = textView.text
+            
+        if text == "\n" {
+            textView.resignFirstResponder()
+                
+            return false
+        }
+            
+        return true
+    }
 
 }
 
+//@IBAction func sendMail(_ sender: Any) {
+//    let picker = MFMailComposeViewController()
+//    picker.mailComposeDelegate = self
+//
+//    if let subjectText = subject.text {
+//        picker.setSubject(subjectText)
+//    }
+//    picker.setMessageBody(body.text, isHTML: true)
+//
+//    present(picker, animated: true, completion: nil)
+//}
+//
+//// MFMailComposeViewControllerDelegate
+//
+//// 1
+//func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+//     dismiss(animated: true, completion: nil)
+//}
+//
+//// UITextFieldDelegate
+//
+//// 2
+//func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//    textField.resignFirstResponder()
+//
+//    return true
+//}
+//
+//// UITextViewDelegate
+//
+//// 3
+//func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+//    body.text = textView.text
+//
+//    if text == "\n" {
+//        textView.resignFirstResponder()
+//
+//        return false
+//    }
+//
+//    return true
+//}
