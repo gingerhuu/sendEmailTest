4e1d73b16223bb0a9bb3e9a0e659063cf2c451cd
Ginger <gingerhusunny@gmail.com>
2020-07-16 09:06:24 -0700

    Initial Commit

---
+++ sendEmailTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sendEmailTest
+//
+//  Created by Ginger Hu on 7/16/20.
+//  Copyright © 2020 Ginger Hu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
